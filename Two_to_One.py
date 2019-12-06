eabe3efce5d24ef88b401fa05bea7526ee2733d6
ttommytrinh <57840872+ttommytrinh@users.noreply.github.com>
2019-12-06 17:23:58 -0600

    7 kyu - Two to One

--- Two_to_One.py
+++ Two_to_One.py
@@ -1,5 +1,6 @@
 '''
-Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters
+Take 2 strings s1 and s2 including only letters from a to z. 
+Return a new sorted string, the longest possible, containing distinct letters
 '''
 
 def longest(s1, s2):
