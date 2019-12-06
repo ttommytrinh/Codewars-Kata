df9854b665d3927b39e83b645b420d200d9b52ac
ttommytrinh <57840872+ttommytrinh@users.noreply.github.com>
2019-12-06 17:21:11 -0600

    6 kyu - Convert string to camel case

--- Convert_String_to_Camel_Case.py
+++ Convert_String_to_Camel_Case.py
@@ -1,7 +1,7 @@
 '''
 Complete the method/function so that it converts dash/underscore delimited words into camel casing.
 The first word within the output should be capitalized only if the original word was capitalized
-(known as Upper Camel Case, also often referred to as Pascal case).
+(known as Upper Camel Case/Pascal case).
 '''
 
 def to_camel_case(text):
