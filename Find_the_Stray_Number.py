a975a85537dc861e58fb2765a22507dc4f0b7497
ttommytrinh <57840872+ttommytrinh@users.noreply.github.com>
2019-12-06 22:20:14 -0600

    7 kyu - Find the stray number

---
+++ Find_the_Stray_Number.py
@@ -0,0 +1,13 @@
+'''
+You are given an odd-length array of integers, in which all of them are the same, 
+except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+'''
+
+def stray(arr):
+    for x in arr:
+        if arr.count(x) == 1:
+            return(x)
