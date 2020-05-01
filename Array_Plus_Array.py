679247790f6a383b2cd2eac00149e7ba42e60220
Tommy Trinh <57840872+ttommytrinh@users.noreply.github.com>
2020-05-01 16:13:23 -0500

    8 kyu - Array plus array

---
+++ Array_Plus_Array.py
@@ -0,0 +1,7 @@
+'''
+Get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+'''
+def array_plus_array(arr1,arr2):
+    return(sum(arr1)+sum(arr2))
