d0ab058621f3bb944f29e9e720faf69996976d59
Tommy Trinh <57840872+ttommytrinh@users.noreply.github.com>
2020-12-29 21:39:40 -0600

    7 kyu - Isograms

---
+++ Isograms.py
@@ -0,0 +1,11 @@
+"""
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters 
+is an isogram. Assume the empty string is an isogram. Ignore letter case.
+"""
+
+def is_isogram(string):
+    for x in string:
+        if string.lower().count(x) > 1:
+            return False
+    return True
